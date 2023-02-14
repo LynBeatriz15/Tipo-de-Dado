aadd0f7a68aaad128881afc457b032639a5f3e1e
LynBeatriz15 <evelynbeatrizmoscheto@gmail.com>
2023-02-14 14:41:48 +0000

    salvar

--- Operadores.java
+++ Operadores.java
@@ -1,3 +1,15 @@
 public class Operadores {
+    public static void main(String[] args){
+        int x = 9;
+        int y = 2;
     
-}
+        System.out.printf("x + y = %d\n", x + y);
+        System.out.printf("x - y = %d\n", x - y);
+        System.out.printf("x * y = %d\n", x * y);
+        System.out.printf("x / y = %d\n", x / y); // Divisão Somente Inteira
+        System.out.printf("x / y = %2.2f\n", x / (double)y); // Divisão float
+        System.out.printf("x + y = %d\n", x % y); //Divisão Resto
+        
+        }
+    }
+
