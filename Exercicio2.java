3a335d3c91324c1b27aad016a4b15776a2c5bed3
LynBeatriz15 <evelynbeatrizmoscheto@gmail.com>
2023-03-06 11:41:58 +0000

    Salvar

---
+++ Exercicio2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        Double Altura, Peso, Imc;
+        
+        System.out.println("Informe sua Altura? ");
+        Altura = input.nextDouble();
+        
+        System.out.println("Informe seu peso ");
+        Peso = input.nextDouble();
+        
+
+    Imc = Peso / (Altura * Altura);
+
+        System.out.printf("Seu Imc é igual a " + (Double)Imc + "\n");
+        
+        if (Imc < 18.5) {
+            System.out.printf ( "\n Abaixo do peso");
+        } else if (Imc >= 18.5 && Imc <= 24.9) {
+            System.out.printf  (" Peso Normal");
+        } else if (Imc >= 25.0 && Imc <=29.9 ) {
+            System.out.printf ( " Obesidade grau 1");
+        } else if (Imc >= 30.0 && Imc<= 34.9) {
+            System.out.printf ("Obesidade grau 2");
+        } else {
+            System.out.printf ("Obesidade grau 3 (mórbida)");
+        }
+    }
+}
